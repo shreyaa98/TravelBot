2a5337b99cfc1f90c3ac8ac9653ce81df3e290bf
Shreyaasri Prakash <33647234+shreyaa98@users.noreply.github.com>
2023-11-01 20:57:41 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,1 @@
-print (" Travel Bot")
+print ("Hi, I am Zee, a Travel Bot to explore and experience various events at your travel destination. Which city would you like to visit(eg: Berlin, Frankfurt, Paris)?")
